b99bf95c13d811fa05519121c5f9a615cdcf7904
Ronald Mutua <ronaldmutua617@gmail.com>
2023-06-14 09:26:52 -0700

    task 2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
     else:
         print("{} arguements:".format(len(sys.argv)-1))
     for i in range(len(sys.argv)):
-            if i == 0:
-                continue
-            else:
-                print("{}: {}".format(i,sys.argv[i]))
+        if i > 0:
+            print("{}: {}".format(i, sys.argv[i]))
