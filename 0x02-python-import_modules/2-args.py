aa2b2a0a3a7bb1b33c651f0136850d618ec36946
Ronald Mutua <ronaldmutua617@gmail.com>
2023-06-14 13:58:40 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print("{} arguements:".format(len(sys.argv)-1))
+        print("{} arguments:".format(len(sys.argv)-1))
     for i in range(len(sys.argv)):
         if i > 0:
             print("{}: {}".format(i, sys.argv[i]))
