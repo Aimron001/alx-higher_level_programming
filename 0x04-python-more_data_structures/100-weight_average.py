d6c46ab6fdc195292ea4541a4e05c76776764a60
Ronald Mutua <ronaldmutua617@gmail.com>
2023-06-14 14:43:33 -0700

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    sum1 =  0
+    sum2 = 0
+
+    for tup in my_list:
+        sum1 += tup[0] * tup[1]
+        sum2 += tup[1]
+
+    return (sum1 / sum2)
