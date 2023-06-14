8966cfd6f229963d17a73c9a09129d719db5f4c6
Ronald Mutua <ronaldmutua617@gmail.com>
2023-06-14 14:24:52 -0700

    task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """deletes a key in a dictionary
+    Args:
+        a_dictionary: the dictionary
+        key: key to delete
+    Returns:
+        modified dictionary
+    """
+    if a_dictionary.get(key) != None:
+        a_dictionary.pop(key)
+    
+    return a_dictionary
