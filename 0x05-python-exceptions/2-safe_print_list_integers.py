ccf030402d15e6f4c419e71749ef95a2eb8c9db1
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-26 10:10:15 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list and only integers
+    Args:
+        my_list: The list to
+        x (int): number of elements
+    Returns:
+        number of elements printed.
+    """
+    count  = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (count)
