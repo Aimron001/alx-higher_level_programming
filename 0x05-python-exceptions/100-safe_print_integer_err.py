60cb833a8f009ddd4a4ac71ea614049a8e87c578
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-26 10:31:47 -0700

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer
+
+    Args:
+        value: integer to print.
+
+    Returns:
+        Returns True if value has been correctly printed
+        Otherwise, returns False and prints in stderr the error
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
