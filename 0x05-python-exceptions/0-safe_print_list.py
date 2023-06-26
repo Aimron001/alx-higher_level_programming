14cd065de0fe13613f7aa49140b0c98663381151
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-26 09:51:34 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+    Args:
+        my_list: list to print elements from
+        x : number of elements to print
+    Returns:
+        The number of elementss printed
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return (count)
