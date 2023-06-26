f1278e97a89b91c1b8f84540c93384b00bbe4546
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-26 10:38:10 -0700

    task 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """does some magic calculation
+
+        args:
+            a: val 1
+            b: val 2
+        
+        Returns:
+            results of the calculation
+    """
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
