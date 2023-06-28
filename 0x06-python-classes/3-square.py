991c3ef14eb710af07a862050872f6bb79d55b1c
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-28 14:09:51 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Defines a square with a size attribute"""
+
+
+class Square:
+    """square class with size attribute
+
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+    def area(self):
+        """ Calculates the area of a square"""
+        return self.__size * self.__size
