3ca79e0e57c69f4cd223b4bbf75647b3de85d1ed
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-28 10:52:47 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines a square with a size attribute"""
+
+
+class Square:
+    """square class with size attribute
+
+    args:
+
+    attributes:
+        size - private instance attribute
+    """
+
+    def __init__(self, size):
+        self.__size = size
