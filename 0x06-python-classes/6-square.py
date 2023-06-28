0bb5896c6a9096e0759ef311f9874690e6abfcd5
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-28 15:08:29 -0700

    task 6 correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,10 +35,10 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.size == 0:
             print()
+            return
+
         for i in range(0, self.__position[1]):
             print("")
         for i in range(0, self.__size):
