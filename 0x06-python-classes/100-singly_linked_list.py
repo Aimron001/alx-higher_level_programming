da08749f7a5e4252e2b216195911bbeb9aa71f00
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-28 14:15:01 -0700

    task 100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines class for  singly-linked list."""
+
+
+class Node:
+    """Represents a node in singly-linked list"""
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Get/set the data of the Node."""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
