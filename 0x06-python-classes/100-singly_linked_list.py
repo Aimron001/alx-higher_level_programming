724366653f3f400b31242cf5ca0398426461abd8
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-28 15:25:35 -0700

    singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -30,3 +30,40 @@ class Node:
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+
+class SinglyLinkedList:
+    """class for a singly-linked list."""
+
+    def __init__(self):
+        """Initalizing a  SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Inserts a new Node to the List.
+
+        Args:
+            value (Node): The new Node to be inserted.
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while (temp.next_node is not None and temp.next_node.data < value):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def __str__(self):
+        """Defines representation of a SinglyLinkedList."""
+        values = []
+        temp = self.__head
+        while temp is not None:
+            values.append(str(temp.data))
+            temp = temp.next_node
+        return ('\n'.join(values))
