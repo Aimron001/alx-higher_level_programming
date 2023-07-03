eccaa6a4adf1c21e01643d51c7e93923e1f585cd
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-07-03 13:45:00 -0700

    task 5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -70,3 +70,7 @@ class Rectangle:
         area_rect = "Rectangle(" + str(self.__width)
         area_rect += ", " + str(self.__height) + ")"
         return (area_rect)
+
+    def __del__(self):
+        """prints bye rectangle when an object is deleted"""
+        print("Bye rectangle...")
