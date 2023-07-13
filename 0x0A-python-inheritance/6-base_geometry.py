c5a5b6e983070a40129eddbf58cb34ece4f404e9
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-07-12 19:20:52 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a base geometry."""
+
+    def area(self):
+        """Not yet  implemented."""
+        raise Exception("area() is not implemented")
