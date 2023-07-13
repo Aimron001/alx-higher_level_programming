07ce7685bfbe236e13fb67959d8e1c7d6f6e2a14
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-07-12 19:13:48 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+3-is_kind_of_class.py
+#!/usr/bin/python3
+"""Defines an inherited function of checking class."""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance inherited from a class.
+
+    Args:
+        obj (any): The object to be checked.
+        a_class (type): The class to match the type of object to.
+    Returns:
+        True if the object is an instance inherited
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
