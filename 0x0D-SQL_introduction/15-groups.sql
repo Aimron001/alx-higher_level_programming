a42a55d895b4500311bbab2bdb6e477a5c6fc632
Ronald Mwau <ronaldmutua617@gmail.com>
2023-08-15 13:33:42 -0700

    corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- counts records with same score
 SELECT score, COUNT(1) AS number FROM second_table
 GROUP BY score
-ORDER BY numebr DESC;
+ORDER BY number DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,7 @@
 -- creates a table and adds multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- inserting the rows
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8)
