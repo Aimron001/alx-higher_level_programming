1f4773e5dacd28f3c07a7e4bc2528cac96197814
Ronald Mwau <ronaldmutua617@gmail.com>
2023-08-19 06:49:55 -0700

    Validating attributes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base:
     """represents the Base class"""
     __nb_objects = 0
     def __init__(self, id=None):
-        if id:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
