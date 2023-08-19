badbf6694c360af4ed1e4b5146ee113972dbd421
Ronald Mwau <ronaldmutua617@gmail.com>
2023-08-19 07:24:16 -0700

    str mtd

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,5 +72,5 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}"
-    .format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.__x, self.__y, self.__width, self.__height))
