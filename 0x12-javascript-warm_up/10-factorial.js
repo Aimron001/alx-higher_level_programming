9b792c9bb5f2e999a101ec2b1d23cc32a0f5fca3
Ronald Mwau <ronaldmutua617@gmail.com>
2023-09-11 11:53:17 -0700

    a;;

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (num < 0) {
+    return (-1);
+  }
+  if (num === 0 || isNaN(num)) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function add (a, b) {
+  const result = a + b;
+  console.log(result);
+}
+
+add(Number(process.argv[2]), Number(process.argv[3]));
