65b9538d26d2b641aa4a0b489ee8e4a427420dbf
Ronald Mutua <ronaldmutua617@gmail.com>
2023-09-29 13:26:20 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status using requests"""
+import requests
+
+url = "https://alx-intranet.hbtn.io/status"
+
+response = requests.get(url)
+if response.status_code -- 200:
+    data = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
